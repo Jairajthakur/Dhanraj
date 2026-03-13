df25d4d2440babb2d50ef93399c3e5b08da476b7
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-13 17:52:52 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,9 +1,13 @@
 module.exports = ({ config }) => {
   const androidConfig = { ...config.android };
-
   if (process.env.GOOGLE_SERVICES_JSON) {
     androidConfig.googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
   }
-
-  return { ...config, android: androidConfig };
+  return {
+    ...config,
+    android: androidConfig,
+    extra: {
+      apiUrl: "https://dhanraj-production-09c4.up.railway.app",
+    },
+  };
 };
