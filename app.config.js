e61f0a63cca6be014d2ec48260b156948e10abad
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-13 19:17:53 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -7,7 +7,11 @@ module.exports = ({ config }) => {
     ...config,
     android: androidConfig,
     extra: {
+      ...config.extra,
       apiUrl: "https://dhanraj-production-09c4.up.railway.app",
+      eas: {
+        projectId: "b08c1d58-d13e-4200-af9b-710e2bc10fb0",
+      },
     },
   };
 };
