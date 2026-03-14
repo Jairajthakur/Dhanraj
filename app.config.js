9592656940e9617467c7a54029f9d0310121cc2c
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-14 08:44:16 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -10,7 +10,7 @@ module.exports = ({ config }) => {
       ...config.extra,
       apiUrl: "https://dhanraj-production-09c4.up.railway.app",
       eas: {
-        projectId: "",
+        projectId: "1b09251a-4423-4759-a22b-fc2f0a44fd8e",
       },
     },
   };
