6da18913aee9195129864913e53d5a3a2e5ced7e
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-14 20:12:15 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,14 +1,15 @@
 module.exports = ({ config }) => {
   const androidConfig = { ...config.android };
-  if (process.env.GOOGLE_SERVICES_JSON) {
-    androidConfig.googleServicesFile = process.env.GOOGLE_SERVICES_JSON;
-  }
+
+  // Always set google-services.json path for FCM to work
+  androidConfig.googleServicesFile = process.env.GOOGLE_SERVICES_JSON || "./google-services.json";
+
   return {
     ...config,
     android: androidConfig,
     extra: {
       ...config.extra,
-      "apiUrl": "https://dhanraj-production.up.railway.app",
+      apiUrl: "https://dhanraj-production.up.railway.app",
       eas: {
         projectId: "1b09251a-4423-4759-a22b-fc2f0a44fd8e",
       },
