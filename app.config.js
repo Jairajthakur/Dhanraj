be87516c36fceea0afcdad099b006cbcdc066fa7
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-14 12:46:01 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ module.exports = ({ config }) => {
     android: androidConfig,
     extra: {
       ...config.extra,
-      apiUrl: "https://dhanraj-production-09c4.up.railway.app",
+      "apiUrl": "https://dhanraj-production.up.railway.app",
       eas: {
         projectId: "1b09251a-4423-4759-a22b-fc2f0a44fd8e",
       },
