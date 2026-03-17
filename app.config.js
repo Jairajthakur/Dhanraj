a350aabe94750c9afbd3c7c8fda2996d863ca4c6
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-17 17:58:59 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,15 +1,18 @@
-module.exports = ({ config }) => ({
-  ...config,
-  web: {
-    bundler: "webpack",
-    output: "static",
-    baseUrl: "/Dhanraj",
-  },
-  extra: {
-    ...config.extra,
-    apiUrl: "https://dhanraj-production.up.railway.app",
-    eas: {
-      projectId: "1b09251a-4423-4759-a22b-fc2f0a44fd8e",
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    platforms: ["ios", "android", "web"],
+    web: {
+      bundler: "metro",
+      output: "static",
+      baseUrl: "/Dhanraj",
     },
-  },
-});
+    extra: {
+      ...config.extra,
+      apiUrl: "https://dhanraj-production.up.railway.app",
+      eas: {
+        projectId: "1b09251a-4423-4759-a22b-fc2f0a44fd8e",
+      },
+    },
+  };
+};
