586dbf28b9c2d91cc071c939684124abfd17422d
Jairaj Singh <118998121+Jairajthakur@users.noreply.github.com>
2026-03-23 17:15:50 +0530

    Update caseStore.ts

--- lib/caseStore.ts
+++ lib/caseStore.ts
@@ -1,15 +1,6 @@
-// lib/caseStore.ts
-// Simple global store to pass case data between screens
-// Avoids Expo Router URL param size limits
-
 let _selectedCase: any = null;
 
 export const caseStore = {
-  set: (item: any) => {
-    _selectedCase = item;
-  },
+  set: (item: any) => { _selectedCase = item; },
   get: () => _selectedCase,
-  clear: () => {
-    _selectedCase = null;
-  },
-};
+  clear: () => { _selectedCase = null; },
